306515b5d1978a7ab6de32072ad47fa1dd32bdcf
1138-4EB <1138-4EB@users.noreply.github.com>
2017-02-17 02:27:30 +0100

    Try to add nightly properly

--- dist/linux/travis-ci.sh
+++ dist/linux/travis-ci.sh
@@ -6,7 +6,8 @@ PKG_SHORTCOMMIT="$(echo $TRAVIS_COMMIT | cut -c1-10)"
 PKG_VER=`grep Ghdl_Ver src/version.in | sed -e 's/.*"\(.*\)";/\1/'`
 PKG_TAG="$TRAVIS_TAG"
 
-if [ -z "$BUILDTHIS" ]; then BUILDTHIS=$(echo "$TRAVIS_TAG"); fi
+BUILDTHIS=`expr 1 - $TAGONLY`;
+if [ "$BUILDTHIS" -eq 0 ]; then BUILDTHIS=$(echo "$TRAVIS_TAG"); fi
 if [ -z "$TRAVIS_TAG" ]; then PKG_TAG=`date -u +%Y%m%d`; fi
 
 export PKG_FILE="ghdl-$PKG_VER-$DBLD-$PKG_TAG-$PKG_DTAG-$PKG_SHORTCOMMIT.tgz"
